015503bd8672f56a3ae3110d1a184fd259adc3c3
tygm <18806524037@163.com>
2018-04-26 17:37:25 +0800

    tygm

--- ibest/src/main/java/com/ty/ibest/controller/UploadController.java
+++ ibest/src/main/java/com/ty/ibest/controller/UploadController.java
@@ -1,15 +1,39 @@
 package com.ty.ibest.controller;
 
 import java.io.File;
+
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.qiniu.util.Auth;
+import com.ty.ibest.constant.InfoConstant;
+import com.ty.ibest.entity.User;
+import com.ty.ibest.utils.Results;
+
 @Controller
-public class UploadController {
+public class UploadController extends BaseController {
+	@RequestMapping(value="/qiniu/token",method =RequestMethod.GET)
+	@ResponseBody
+	public Results<String> adminLogin(){
+		String openId = null;
+		User user = null;
+		String accessKey = "SCzBwqATeo83cDUtz4PLw6IRzPazceJyaNYDuBSf";
+		String secretKey = "es0JnIV2vd08Ns428JUHcXSOKbcvKyYk0tPs6ug9";
+		String bucket = "tygm";
+		Auth auth = Auth.create(accessKey, secretKey);
+		String upToken = auth.uploadToken(bucket);
+		System.out.println(upToken);
+		return successResult(upToken);
+		
+		
+	}
 	//E:\\javademo\\ibest\\src\\main\\webapp\\
 	private static final String PATH="E:\\image\\";
 	@RequestMapping(value="/img/upload",method = RequestMethod.POST)
